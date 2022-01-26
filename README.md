362e9ef825761c641c9d921ad10f0916d0473982
Danny Perez <danny@intricatecloud.io>
2022-01-26 01:41:49 -0500

    Adding a README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Milestone 1 Project: Ruffhouse
+
+Refer to the assignment for most of the details of the project
+
+## Usage
+
+* Run  `npm install` to install all the project dependencies
+* Run `npm run start` to start the server
+* Visit http://localhost:8080 to see the web application
--- cypress/integration/e2e-spec.js
+++ cypress/integration/e2e-spec.js
@@ -4,7 +4,7 @@ describe("end to end tests", () => {
     cy.get(".navbar-burger").click();
     cy.get(".navbar-item > .button").click();
     // confirm the application is showing two items
-    cy.get("#login").type("danny@intricatecloud.io");
+    cy.get("#login").type("cs5356@class.com");
     cy.get('[type="password"]').type("Demo1234");
     cy.get("form > button").click();
 
