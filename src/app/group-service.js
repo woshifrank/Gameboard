6d1b87769b3b0a86db77e96e2c9b5bd15e418ca2
woshifrank <frankyyj98@gmail.com>
2022-04-24 12:16:37 -0400

    Group creation logistic complete

--- src/app/group-service.js
+++ src/app/group-service.js
@@ -7,28 +7,109 @@ if (admin.apps.length === 0) {
 }
 const db = admin.firestore();
 module.exports = {
-    createGroup: async (id,info) => {
-        const docRef = db.collection('groups').doc(id)
-        await docRef.set({
-            group_name: info.group_name, 
-            game_name: info.game_name,
-            game_type: info.game_type, 
-            slogan: info.slogan,
-            intro: info.intro,
-            admin_id: info.admin_id,
-            admin_email: info.admin_email
-        })
-        return false;
-    },
     getGroupById: async (id) =>{
         const doc = await db.collection('groups').doc(id).get()
         if (!doc.exists) {
           console.log('No such document!');
           return null;
         } else {
-          console.log(doc)
+          //console.log(doc)
           return doc.data();
         }
+    },
+    getGroupByName: async (name) => {
+        groupRef = db.collection('groups')
+        const group = await groupRef.where("group_name", "==", name).limit(1).get()
+        if (!group.docs[0]) {
+            console.log('No matching documents.');
+            return null;
+        }else{
+            console.log(group.docs[0].data())
+            return group.docs[0].data()
+        }
+    },
+    // TODO: check duplicate groups
+    createGroup: async (info) => {
+        // check if the group exists
+        groupRef = db.collection('groups')
+        const check = await groupRef.where("group_name", "==", info.group_name).limit(1).get()
+        if(check.docs[0]){
+            console.log('Error, group name exists!')
+            return false;
+        }
+        const create_res = await db.collection('groups').add({
+            group_name: info.group_name, 
+            game_name: info.game_name,
+            game_type: info.game_type, 
+            slogan: info.slogan,
+            intro: info.intro,
+            admin_email: info.admin_email,
+            user_emails:[info.admin_email]
+        })
+        const userRef = db.collection('users')
+        const user_data = await userRef.where("email", "==", info.admin_email).limit(1).get()
+
+        user = user_data.docs[0]
+        let user_group = user.data().player_group_ids
+        let admin_group = user.data().admin_group_ids
+        if (!user_group){
+            new_user_group = [create_res.id];
+            new_admin_group = [create_res.id];
+            await user.ref.update({
+                player_group_ids: new_user_group,
+                admin_group_ids: new_admin_group
+            }).then(function() {
+                console.log("User account update complete for new admin1");
+            });
+        }
+        else{
+            user_group.push(create_res.id)
+            if (!admin_group){
+                new_admin_group = [create_res.id]
+            }
+            else{
+                new_admin_group = admin_group
+                new_admin_group.push(create_res.id)
+            }
+            await user.ref.update({
+                player_group_ids: user_group,
+                admin_group_ids: new_admin_group
+            }).then(function() {
+                console.log("User account update complete for new admin2");
+            });
+        }
+        /*
+        user.forEach(doc => {
+            let user_group = doc.data().player_group_ids
+            let admin_group = doc.data().admin_group_ids
+            if (!user_group){
+                new_user_group = [create_res.id];
+                new_admin_group = [create_res.id];
+                const res = await doc.ref.update({
+                    player_group_ids: new_user_group,
+                    admin_group_ids: new_admin_group
+                }).then(function() {
+                    console.log("User account update complete for new admin1");
+                });
+            }
+            else{
+                user_group.push(create_res.id)
+                if (!admin_group){
+                    new_admin_group = [create_res.id]
+                }
+                else{
+                    new_admin_group = admin_group
+                    new_admin_group.push(create_res.id)
+                }
+                const res = await doc.ref.update({
+                    player_group_ids: user_group,
+                    admin_group_ids: new_admin_group
+                }).then(function() {
+                    console.log("User account update complete for new admin2");
+                });
+            }
+        });*/
+        return true;
     }
 }
 // userData.email
--- src/app/group-service.test.js
+++ src/app/group-service.test.js
@@ -1,17 +1,28 @@
 const GroupService = require('./group-service');
+const UserService = require('./group-service');
 
 describe('GroupService', () => {
     it('creates group', async () => {
-        group_info = {group_name:'Skyrim', 
-            game_name: 'The Elder Scrolls V: Skyrim',
-            game_type:'RPG', 
-            slogan:'Welcome Dragonborns',
-            intro:'Here houses the discussion channels for official annoucements, game storysharing, walkthroughs, meme sharing and game feedback',
-            admin_id: 'WLTn2CaQq1eIDZtKsPitJmUBphB3',
-            admin_email: 'yyjyang4@umich.edu'};
-        await GroupService.createGroup('1',group_info)
-        const group = await GroupService.getGroupById('1')
-        expect(group.admin_email).toEqual('yyjyang4@umich.edu')
-        expect(group.game_type).toEqual('RPG')
+        group_info = {group_name:'CSGO-Mods', 
+            game_name: 'Counter-Strike: Global Offensive',
+            game_type:'FPS', 
+            slogan:'Welcome, MOD makers and users!',
+            intro:'Here houses the discussion channels CSGO MOD making',
+            admin_email: 'yy586@cornell.edu'};
+        const res = await GroupService.createGroup(group_info)
+
+        // const group = await GroupService.getGroupByName('Skyrim')
+
+        /*
+        const group = await GroupService.getGroupByName('CSGO')
+        //expect(group.game_name).toEqual('The Elder Scrolls V: Skyrim')
+        console.log(group)
+        */
+
+        //const group = await GroupService.getGroupById('1')
+        //expect(group.admin_email).toEqual('yyjyang4@umich.edu')
+        //expect(group.game_type).toEqual('RPG')
+        // const user = await UserService.getUserById('WLTn2CaQq1eIDZtKsPitJmUBphB3')
+        //console.log(user)
     })
 })
